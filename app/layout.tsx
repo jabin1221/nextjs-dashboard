c048d2c512115ec9d4417facb531b271449301ae
jabin1221 <koreanjb1221@gmail.com>
2024-07-30 15:25:41 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// #1-1. Styling: 전역 CSS 적용
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+
+
+export default function RootLayout({children,}: {children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* #2-1-2 body 태그에 inter 폰트 적용*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
